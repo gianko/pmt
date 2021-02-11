6e018a49c6de7c6769e054cfb4b3c4c4cb33ab00
Gian F. Olivieri <gian.olivieri@mediamonks.com>
2021-02-11 02:39:39 -0300

    this maybe fixes the weird promises

--- twitterbot/src/db/index.ts
+++ twitterbot/src/db/index.ts
@@ -7,17 +7,18 @@ export const openDB = (): Promise<any> => new Promise((resolve, reject) => {
   })
 });
 
-export const countPosts = (): Promise<number> => openDB().then((db) => 
-  { 
+export const countPosts = (): Promise<number> => openDB().then((db) => {
   db.get('SELECT COUNT(*) FROM post', [], (err: any, res: any) => {
-    console.log({err, res});
+    console.log({ err, res });
     db.close();
     if (err) return Promise.reject(err);
     return Promise.resolve(res['COUNT(*)'] as number);
-    })}); 
-    
+  });
+  return Promise.reject('but what?');
+});
 
-export const savePost = ({ id_str }: { id_str: string }): Promise<void> => openDB().then((db) =>
+
+export const savePost = ({ id_str }: { id_str: string }): Promise<void> => openDB().then((db) => {
   db.run(
     'INSERT INTO post (tweet_id, timestamp) VALUES ($id, $date)',
     { $id: id_str, $date: Date.now() },
@@ -25,19 +26,24 @@ export const savePost = ({ id_str }: { id_str: string }): Promise<void> => openD
       db.close();
       if (err) return Promise.reject(err);
       return Promise.resolve();
-    }));
+    });
+  return Promise.reject('but how?');
+});
 
-export const handleError = (error: string, index: number = 0): Promise<void> => openDB().then((db) => db.run(
-  'INSERT INTO error (timestamp, index_number, error) VALUES ($date, $index, $error)',
-  {
-    $date: Date.now(),
-    $error: error,
-    $index: index,
-  },
-  (err: any) => {
-    db.close();
-    // tslint:disable-next-line no-console
-    console.error(error);
-    if (err) return Promise.reject(err);
-    return Promise.resolve();
-  }));
+export const handleError = (error: string, index: number = 0): Promise<void> => openDB().then((db) => {
+  db.run(
+    'INSERT INTO error (timestamp, index_number, error) VALUES ($date, $index, $error)',
+    {
+      $date: Date.now(),
+      $error: error,
+      $index: index,
+    },
+    (err: any) => {
+      db.close();
+      // tslint:disable-next-line no-console
+      console.error(error);
+      if (err) return Promise.reject(err);
+      return Promise.resolve();
+    });
+  return Promise.reject('but why?');
+});
