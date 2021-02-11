525a74fb66e74ae23597441782a988ca2bd735ea
Gian F. Olivieri <gian.olivieri@mediamonks.com>
2021-02-11 01:58:28 -0300

    db cositas

--- twitterbot/src/db/index.ts
+++ twitterbot/src/db/index.ts
@@ -1,9 +1,36 @@
-export const countPosts = (): Promise<number> => Promise.resolve(0);
+const sqlite3 = require('sqlite3').verbose();
 
-export const savePost = ({ id_str }: { id_str: string }): Promise<void> => Promise.resolve();
+export const openDB = (): Promise<any> => new Promise((resolve, reject) => {
+  const db = new sqlite3.Datebase('../papitadb', (err: any) => {
+    if (err) return reject(err);
+    return resolve(db);
+  })
+});
 
-export const handleError = (error: string): Promise<void> => {
-  // tslint:disable-next-line no-console
-  console.error(error);
-  return Promise.resolve();
-}
+export const countPosts = (): Promise<number> => openDB().then((db) =>
+  db.get('SELECT COUNT(*) FROM post', [], (err: any, res: any) => {
+    db.close();
+    if (err) return Promise.reject(err);
+    return Promise.resolve(res['COUNT(*)']);
+  }));
+
+export const savePost = ({ id_str }: { id_str: string }): Promise<void> => openDB().then((db) =>
+  db.run(
+    'INSERT INTO post (tweet_id, timestamp) VALUES ($id, $date)',
+    { $id: id_str, $date: Date.now() },
+    (err: any) => {
+      db.close();
+      if (err) return Promise.reject(err);
+      return Promise.resolve();
+    }));
+
+export const handleError = (error: string): Promise<void> => openDB().then((db) => db.run(
+  'INSERT INTO error () VALUES ()',
+  {},
+  (err: any) => {
+    db.close();
+    // tslint:disable-next-line no-console
+    console.error(error);
+    if (err) return Promise.reject(err);
+    return Promise.resolve();
+  }));
